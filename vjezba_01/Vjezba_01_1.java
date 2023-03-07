0a0876187ae6ee1d2df66f59db703cb19b4bb872
Petar M <pmatisic@foi.hr>
2023-03-07 01:46:38 +0100

    Rad od kuće

--- vjezba_01/Vjezba_01_1.java
+++ vjezba_01/Vjezba_01_1.java
@@ -1,7 +1,7 @@
 public class Vjezba_01_1 {
-public static void main(String[] args) {
-for(String a : args) {
-System.out.print(a+"\n");
-}
-}
+	public static void main(String[] args) {
+		for(String a : args) {
+			System.out.print(a+"\n");
+		}
+	}
 }
