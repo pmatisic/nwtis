181328d2adf23adb89e13b410149da56c2dc49d9
Petar M <pmatisic@foi.hr>
2023-04-04 16:59:54 +0200

    Vjezba 04

---
+++ pmatisic_vjezba_04/pmatisic_vjezba_04_1/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module pmatisic_vjezba {
+  requires lombok;
+  requires java.logging;
+  requires com.google.gson;
+
+  opens org.foi.nwtis.pmatisic.vjezba_04.podaci;
+}
---
+++ pmatisic_vjezba_04/pmatisic_vjezba_04_1/src/main/java/pmatisic_vjezba_04_1/vjezba_04/podaci/package-info.java
@@ -0,0 +1,1 @@
+package pmatisic_vjezba_04_1.vjezba_04.podaci;
