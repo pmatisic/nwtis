7274766d096af1f80459d80caaa8ac7e9d20cbfa
Petar M <pmatisic@foi.hr>
2023-06-18 19:47:15 +0200

    [Projekt] app4 završena

--- pmatisic-projekt/pmatisic_aplikacija_4/src/main/java/org/foi/nwtis/pmatisic/projekt/servis/WsLetovi.java
+++ pmatisic-projekt/pmatisic_aplikacija_4/src/main/java/org/foi/nwtis/pmatisic/projekt/servis/WsLetovi.java
@@ -5,11 +5,11 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.foi.nwtis.Konfiguracija;
-import org.foi.nwtis.pmatisic.projekt.entitet.Airports;
 import org.foi.nwtis.pmatisic.projekt.iznimka.PogresnaAutentikacija;
 import org.foi.nwtis.pmatisic.projekt.zrno.AirportFacade;
 import org.foi.nwtis.pmatisic.projekt.zrno.KorisniciFacade;
 import org.foi.nwtis.pmatisic.projekt.zrno.LetoviPolasciFacade;
+import org.foi.nwtis.rest.klijenti.NwtisRestIznimka;
 import org.foi.nwtis.rest.klijenti.OSKlijentBP;
 import org.foi.nwtis.rest.podaci.LetAviona;
 import jakarta.inject.Inject;
@@ -68,53 +68,56 @@ public class WsLetovi {
   public List<LetAviona> dajPolaskeNaDan(@WebParam String korisnik, @WebParam String lozinka,
       @WebParam String icao, @WebParam String dan, @WebParam Integer odBroja,
       @WebParam Integer broj) throws Exception {
-    if (korisniciFacade.autenticiraj(korisnik, lozinka)) {
-      if (odBroja < 1 || broj < 1) {
-        odBroja = 1;
-        broj = 20;
-      }
-      int offset = (odBroja - 1) * broj;
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-      LocalDate datum = null;
-      try {
-        datum = LocalDate.parse(dan, formatter);
-      } catch (Exception e) {
-        throw new Exception("Neispravan format datuma. Molimo koristite format dd.MM.yyyy.");
-      }
-      return lpFacade.dohvatiLetoveNaDan(icao, datum, offset, broj);
-    } else {
+    if (!korisniciFacade.autenticiraj(korisnik, lozinka)) {
       throw new PogresnaAutentikacija("Pogrešno korisničko ime ili lozinka.");
     }
+
+    if (odBroja < 1 || broj < 1) {
+      odBroja = 1;
+      broj = 20;
+    }
+
+    int offset = (odBroja - 1) * broj;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    LocalDate datum = null;
+
+    try {
+      datum = LocalDate.parse(dan, formatter);
+    } catch (Exception e) {
+      throw new Exception("Neispravan format datuma. Molimo koristite format dd.MM.yyyy.");
+    }
+
+    return lpFacade.dohvatiLetoveNaDan(icao, datum, offset, broj);
   }
 
   @WebMethod
   public List<LetAviona> dajPolaskeNaDanOS(@WebParam String korisnik, @WebParam String lozinka,
       @WebParam String icao, @WebParam String dan) throws Exception {
-    if (korisniciFacade.autenticiraj(korisnik, lozinka)) {
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-      LocalDate datum = null;
-      try {
-        datum = LocalDate.parse(dan, formatter);
-      } catch (Exception e) {
-        throw new Exception("Neispravan format datuma. Molimo koristite format dd.MM.yyyy.");
-      }
-      int odVremena = (int) datum.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
-      int doVremena =
-          (int) datum.plusDays(1).atStartOfDay().minusSeconds(1).toEpochSecond(ZoneOffset.UTC);
-      Konfiguracija konfiguracija = (Konfiguracija) konfig.getAttribute("konfiguracija");
-      String korisnikOSN = konfiguracija.dajPostavku("OpenSkyNetwork.korisnik").toString();
-      String ldap = konfiguracija.dajPostavku("aai.ldap").toString();
-      // String lozinka = konfiguracija.dajPostavku("OpenSkyNetwork.lozinka").toString();
-      // OSKlijent osKlijent = new OSKlijent(korisnik, lozinka);
-      OSKlijentBP osKlijent = new OSKlijentBP(ldap, korisnikOSN);
-      List<LetAviona> avioniPolasci = osKlijent.getDepartures(icao, odVremena, doVremena);
-      for (LetAviona let : avioniPolasci) {
-        Airports aerodrom = airportFacade.find(let.getEstDepartureAirport());
-        lpFacade.dodajLet(let, aerodrom);
-      }
-      return avioniPolasci;
-    } else {
+    if (!korisniciFacade.autenticiraj(korisnik, lozinka)) {
       throw new PogresnaAutentikacija("Pogrešno korisničko ime ili lozinka.");
+    }
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    LocalDate parsedDate = null;
+    try {
+      parsedDate = LocalDate.parse(dan, formatter);
+    } catch (Exception e) {
+      throw new Exception("Neispravan format datuma. Molimo koristite format dd.MM.yyyy.");
+    }
+    long odVremena = parsedDate.atStartOfDay(ZoneOffset.UTC).toEpochSecond();
+    long doVremena = parsedDate.plusDays(1).atStartOfDay(ZoneOffset.UTC).toEpochSecond();
+    Konfiguracija konfiguracija = (Konfiguracija) konfig.getAttribute("konfiguracija");
+    String korisnikOSN = konfiguracija.dajPostavku("OpenSkyNetwork.korisnik").toString();
+    String ldap = konfiguracija.dajPostavku("aai.ldap").toString();
+    // String lozinka = konfiguracija.dajPostavku("OpenSkyNetwork.lozinka").toString();
+    // OSKlijent osKlijent = new OSKlijent(korisnik, lozinka);
+    OSKlijentBP osKlijent = new OSKlijentBP(ldap, korisnikOSN);
+
+    try {
+      return osKlijent.getDepartures(icao, odVremena, doVremena);
+    } catch (NwtisRestIznimka e) {
+      e.printStackTrace();
+      throw new Exception("Pogreška pri dohvaćanju podataka s OpenSkyNetwork.");
     }
   }
 
--- pmatisic-projekt/pmatisic_aplikacija_4/src/main/java/org/foi/nwtis/pmatisic/projekt/zrno/LetoviPolasciFacade.java
+++ pmatisic-projekt/pmatisic_aplikacija_4/src/main/java/org/foi/nwtis/pmatisic/projekt/zrno/LetoviPolasciFacade.java
@@ -1,11 +1,9 @@
 package org.foi.nwtis.pmatisic.projekt.zrno;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
-import org.foi.nwtis.pmatisic.projekt.entitet.Airports;
 import org.foi.nwtis.pmatisic.projekt.entitet.LetoviPolasci;
 import org.foi.nwtis.rest.podaci.LetAviona;
 import jakarta.annotation.PostConstruct;
@@ -15,7 +13,6 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 
@@ -24,12 +21,10 @@ public class LetoviPolasciFacade {
 
   @PersistenceContext(unitName = "nwtis_dz3_pu")
   private EntityManager em;
-  private CriteriaBuilder cb;
 
   @PostConstruct
   private void init() {
     System.out.println("LetoviPolasciFacade - init");
-    cb = em.getCriteriaBuilder();
   }
 
   public List<LetAviona> dohvatiLetovePoIntervalu(String icao, LocalDate datumOd, LocalDate datumDo,
@@ -37,25 +32,18 @@ public class LetoviPolasciFacade {
     CriteriaBuilder cb = em.getCriteriaBuilder();
     CriteriaQuery<LetoviPolasci> cq = cb.createQuery(LetoviPolasci.class);
     Root<LetoviPolasci> root = cq.from(LetoviPolasci.class);
-    Join<LetoviPolasci, Airports> airportJoin = root.join("airport");
-    Predicate icaoPred = cb.equal(airportJoin.get("icao"), icao);
-
     long epochDatumOd = datumOd.atStartOfDay(ZoneOffset.UTC).toEpochSecond();
-    long epochDatumDo = datumDo.atStartOfDay(ZoneOffset.UTC).toEpochSecond();
-
+    long epochDatumDo = datumDo.plusDays(1).atStartOfDay(ZoneOffset.UTC).toEpochSecond();
+    Predicate icaoPred = cb.equal(root.get("airport").get("icao"), icao);
     Predicate datumOdPred =
         cb.greaterThanOrEqualTo(root.get("firstSeen").as(Long.class), epochDatumOd);
-    Predicate datumDoPred = cb.lessThan(root.get("lastSeen").as(Long.class), epochDatumDo);
-
+    Predicate datumDoPred = cb.lessThan(root.get("firstSeen").as(Long.class), epochDatumDo);
     cq.where(cb.and(icaoPred, datumOdPred, datumDoPred));
-
     TypedQuery<LetoviPolasci> query = em.createQuery(cq);
     query.setFirstResult(odBroja);
     query.setMaxResults(broj);
-
     List<LetoviPolasci> rezultati = query.getResultList();
     List<LetAviona> letovi = new ArrayList<>();
-
     for (LetoviPolasci lp : rezultati) {
       LetAviona let = new LetAviona();
       let.setIcao24(lp.getIcao24());
@@ -72,18 +60,19 @@ public class LetoviPolasciFacade {
       let.setArrivalAirportCandidatesCount(lp.getArrivalAirportCandidatesCount());
       letovi.add(let);
     }
-
     return letovi;
   }
 
   public List<LetAviona> dohvatiLetoveNaDan(String icao, LocalDate datum, int odBroja, int broj) {
+    CriteriaBuilder cb = em.getCriteriaBuilder();
     CriteriaQuery<LetoviPolasci> cq = cb.createQuery(LetoviPolasci.class);
     Root<LetoviPolasci> root = cq.from(LetoviPolasci.class);
+    long epochDatum = datum.atStartOfDay(ZoneOffset.UTC).toEpochSecond();
+    long epochSutrasnjiDatum = datum.plusDays(1).atStartOfDay(ZoneOffset.UTC).toEpochSecond();
     Predicate icaoPred = cb.equal(root.get("airport").get("icao"), icao);
     Predicate datumOdPred =
-        cb.greaterThanOrEqualTo(root.get("stored"), Timestamp.valueOf(datum.atStartOfDay()));
-    Predicate datumDoPred = cb.lessThanOrEqualTo(root.get("stored"),
-        Timestamp.valueOf(datum.plusDays(1).atStartOfDay()));
+        cb.greaterThanOrEqualTo(root.get("firstSeen").as(Long.class), epochDatum);
+    Predicate datumDoPred = cb.lessThan(root.get("firstSeen").as(Long.class), epochSutrasnjiDatum);
     cq.where(cb.and(icaoPred, datumOdPred, datumDoPred));
     TypedQuery<LetoviPolasci> query = em.createQuery(cq);
     query.setFirstResult(odBroja);
@@ -107,52 +96,6 @@ public class LetoviPolasciFacade {
       letovi.add(let);
     }
     return letovi;
-  }
-
-  public void dodajLet(LetAviona let, Airports airport) {
-    if (let.getEstArrivalAirport() == null) {
-      System.out.println("EstArrivalAirport je null. Let nije dodan.");
-      return;
-    } else if (let.getEstDepartureAirport() == null) {
-      System.out.println("EstDepartureAirport je null. Let nije dodan.");
-      return;
-    } else if (airport == null) {
-      System.out.println("Airport je null. Let nije dodan.");
-      return;
-    } else {
-      if (!icao24FirstSeenPostoje(let.getIcao24(), let.getFirstSeen())) {
-        LetoviPolasci noviLet = new LetoviPolasci();
-        noviLet.setIcao24(let.getIcao24());
-        noviLet.setCallsign(let.getCallsign());
-        noviLet.setFirstSeen(let.getFirstSeen());
-        noviLet.setLastSeen(let.getLastSeen());
-        noviLet.setEstArrivalAirport(let.getEstArrivalAirport());
-        noviLet.setArrivalAirportCandidatesCount(let.getArrivalAirportCandidatesCount());
-        noviLet.setDepartureAirportCandidatesCount(let.getDepartureAirportCandidatesCount());
-        noviLet.setEstArrivalAirportHorizDistance(let.getEstArrivalAirportHorizDistance());
-        noviLet.setEstArrivalAirportVertDistance(let.getEstArrivalAirportVertDistance());
-        noviLet.setEstDepartureAirportHorizDistance(let.getEstDepartureAirportHorizDistance());
-        noviLet.setEstDepartureAirportVertDistance(let.getEstDepartureAirportVertDistance());
-        noviLet.setAirport(airport);
-        noviLet.setStored(new Timestamp(System.currentTimeMillis()));
-        em.persist(noviLet);
-      } else {
-        System.out.println("Let s istim 'icao24' i 'firstSeen' već postoji. Let nije dodan.");
-      }
-    }
-  }
-
-  public boolean icao24FirstSeenPostoje(String icao24, int firstSeen) {
-    cb = em.getCriteriaBuilder();
-    CriteriaQuery<LetoviPolasci> cq = cb.createQuery(LetoviPolasci.class);
-    Root<LetoviPolasci> root = cq.from(LetoviPolasci.class);
-    Predicate firstSeenPred = cb.equal(root.get("firstSeen"), firstSeen);
-    Predicate icao24Pred = cb.equal(root.get("icao24"), icao24);
-    cq.where(cb.and(firstSeenPred, icao24Pred));
-    TypedQuery<LetoviPolasci> q = em.createQuery(cq);
-    q.setMaxResults(1);
-    List<LetoviPolasci> zadnjiLet = q.getResultList();
-    return !zadnjiLet.isEmpty();
   }
 
 }
