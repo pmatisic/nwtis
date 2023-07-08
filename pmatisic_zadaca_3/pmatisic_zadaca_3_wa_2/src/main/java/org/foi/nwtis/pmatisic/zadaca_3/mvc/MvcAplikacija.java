097117ce488e6f58886b9a34ea8712aa61302c3a
Petar Matišić <93230776+pmatisic@users.noreply.github.com>
2023-07-09 01:27:45 +0200

    [AD] Brisanje datoteke

--- pmatisic_zadaca_3/pmatisic_zadaca_3_wa_2/src/main/java/org/foi/nwtis/pmatisic/zadaca_3/mvc/MvcAplikacija.java
+++
@@ -1,7 +0,0 @@
-package org.foi.nwtis.pmatisic.zadaca_3.mvc;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("mvc")
-public class MvcAplikacija extends Application {}
